3887d3805a2378d84865995a4519128cae057553
stanleyselvaratnam <stanley.selvaratnam@master.hes-so.ch>
2025-10-14 21:59:41 +0200

    The config for the raspberrypi4-64

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,12 +1,12 @@
 #/bin/bash
-sudo kpartx -av ../build/tmp/deploy/images/qemuarm64/core-image-minimal-qemuarm64.wic
+sudo kpartx -av ../build/tmp/deploy/images/raspberrypi4-64/core-image-minimal-raspberrypi4-64.sdimg
 
 sudo umount /mnt
 sudo mount /dev/mapper/loop0p1 /mnt && ls /mnt && sudo umount /mnt
 sudo mount /dev/mapper/loop0p2 /mnt && ls /mnt && sudo umount /mnt
 sudo mount /dev/mapper/loop0p3 /mnt && ls /mnt && sudo umount /mnt
 sudo mount /dev/mapper/loop0p4 /mnt && ls /mnt && sudo umount /mnt
-sudo mount /dev/mapper/loop0p5 /mnt && ls /mnt && sudo umount /mnt
-sudo mount /dev/mapper/loop0p6 /mnt && ls /mnt && sudo umount /mnt
+# sudo mount /dev/mapper/loop0p5 /mnt && ls /mnt && sudo umount /mnt
+# sudo mount /dev/mapper/loop0p6 /mnt && ls /mnt && sudo umount /mnt
 
 sudo kpartx -d /dev/loop0
